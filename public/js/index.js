6dfa9b3b49e4cc53c3c64f1b7e1bd3da39c8756a
Light TP <1mpulse.z10n@gmail.com>
2018-03-19 16:18:03 +0700

    Adjust chat box and auto scroll

--- public/js/index.js
+++ public/js/index.js
@@ -8,13 +8,15 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (message) {
-  console.log('New message', message);
-  let li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+$(document).ready(function() {
+  socket.on('newMessage', function (message) {
+    console.log('New message', message);
+    let li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
 
-  jQuery('#messages').append(li);
-});
+    jQuery('#messages').append(li);
+  });
+
 
 // socket.emit('createMessage', {
 //   from: 'Test',
@@ -23,13 +25,14 @@ socket.on('newMessage', function (message) {
 //   console.log('Got it', res);
 // })
 
-jQuery('#message-form').on('submit', function (e) {
-  e.preventDefault();
+  jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    $('#messages').animate({ scrollTop: $('#messages').prop("scrollHeight") }, 2000)
+    socket.emit('createMessage', {
+      from: 'User',
+      text: jQuery('[name=message]').val()
+    }, function () {
 
-  socket.emit('createMessage', {
-    from: 'User',
-    text: jQuery('[name=message]').val()
-  }, function () {
-
-  })
+    })
+  });
 });
